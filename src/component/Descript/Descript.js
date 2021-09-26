dfaf892dfee07aad9f7bb04ca9d112359e07e65e
Jony120455 <jonyh0296@gmail.com>
2021-09-26 21:02:15 +0600

    added all

--- src/component/Descript/Descript.js
+++ src/component/Descript/Descript.js
@@ -6,6 +6,7 @@ const Descript = () => {
     return (
         <div>
             { <h1 style={{color:'#153769'}}><i class="fas fa-globe-asia"></i>Welcome to Bangladesh</h1>}
+            <h1>Total budget is:200 million</h1>
             
         </div>
     );
